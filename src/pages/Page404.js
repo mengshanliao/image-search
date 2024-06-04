ee11f7d65ed98c200de1c658448bd128961bcebd
mengshanliao <r0918103121@gmail.com>
2024-06-04 18:09:03 +0800

    add Page404

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Page404 = () => {
-  return <div>Page404</div>;
+  return (
+    <div className="page404">
+      <Link to="/">Go to Home</Link>
+    </div>
+  );
 };
 
 export default Page404;
